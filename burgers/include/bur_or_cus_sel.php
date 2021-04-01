b0310b01021fa8c0bf028e41342b0ef5f68e31d6
cocoao <cocoaooo212@gmail.com>
2021-04-01 18:34:31 +0900

    210401 custom order 페이지 재료선택-뷰박스 기능 작업중

---
+++ burgers/include/bur_or_cus_sel.php
@@ -0,0 +1,248 @@
+<div class="bun selectBox selectBox_on" value="bun">
+  <div class="select">
+    <img src="/burgers/img/cu_bun_01.png" alt="bun_01">
+    <div class="selHover">통밀번</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_bun_02.png" alt="bun_02">
+    <div class="selHover">참깨번</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_bun_03.png" alt="bun_03">
+    <div class="selHover">호밀번</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_bun_04.png" alt="bun_04">
+    <div class="selHover">먹물번</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_bun_05.png" alt="bun_05">
+    <div class="selHover">먹물참깨번</div>
+  </div>
+</div>
+<div class="patty selectBox selectBox_on" value="patty">
+  <div class="select">
+    <img src="/burgers/img/cu_pat_01.png" alt="patty_01">
+    <div class="selHover">소고기</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_pat_02.png" alt="patty_02">
+    <div class="selHover">돼지고기</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_pat_03.png" alt="patty_03">
+    <div class="selHover">닭고기</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_pat_04.png" alt="patty_04">
+    <div class="selHover">새우</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_pat_05.png" alt="patty_05">
+    <div class="selHover">버섯</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_pat_06.png" alt="patty_06">
+    <div class="selHover">콩</div>
+  </div>
+</div>
+<div class="cheese selectBox selectBox_on" value="cheese">
+  <div class="select">
+    <img src="/burgers/img/cu_ch_01.png" alt="cheese_01">
+    <div class="selHover">아메리칸치즈</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ch_02.png" alt="cheese_02">
+    <div class="selHover">슈레드치즈</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ch_03.png" alt="cheese_03">
+    <div class="selHover">모차렐라치즈</div>
+  </div>
+  <div class="select reset">
+    <img src="/burgers/img/cu_none.png" alt="cheese_none">
+    <div class="selHover">선택안함</div>
+  </div>
+</div>
+<div class="vegetable selectBox selectBox_do" value="vegetable">
+  <div class="select">
+    <img src="/burgers/img/cu_ve_01.png" alt="vegetable_01">
+    <div class="selHover">양상추</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_02.png" alt="vegetable_02">
+    <div class="selHover">로메인</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_03.png" alt="vegetable_03">
+    <div class="selHover">토마토</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_04.png" alt="vegetable_03">
+    <div class="selHover">양파</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_05.png" alt="vegetable_05">
+    <div class="selHover">피망</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_06.png" alt="vegetable_06">
+    <div class="selHover">오이</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_07.png" alt="vegetable_07">
+    <div class="selHover">아보카도</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_ve_08.png" alt="vegetable_08">
+    <div class="selHover">구운양파</div>
+  </div>
+  <div class="select reset">
+    <img src="/burgers/img/cu_none.png" alt="vegetable_none">
+    <div class="selHover">선택안함</div>
+  </div>
+</div>
+<div class="topping selectBox selectBox_do" value="topping">
+  <div class="select">
+    <img src="/burgers/img/cu_to_01.png" alt="topping_01">
+    <div class="selHover">페퍼로니</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_02.png" alt="topping_02">
+    <div class="selHover">베이컨</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_03.png" alt="topping_03">
+    <div class="selHover">해쉬브라운</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_04.png" alt="topping_04">
+    <div class="selHover">써니사이드에그</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_05.png" alt="topping_05">
+    <div class="selHover">스크램블에그</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_06.png" alt="topping_06">
+    <div class="selHover">피클</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_07.png" alt="topping_07">
+    <div class="selHover">할라피뇨</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_to_08.png" alt="topping_08">
+    <div class="selHover">올리브</div>
+  </div>
+  <div class="select reset">
+    <img src="/burgers/img/cu_none.png" alt="topping_none">
+    <div class="selHover">선택안함</div>
+  </div>
+</div>
+<div class="sauce selectBox selectBox_do" value="sauce">
+  <div class="select">
+    <img src="/burgers/img/cu_sa_01.png" alt="sauce_01">
+    <div class="selHover">머스타드</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_02.png" alt="sauce_02">
+    <div class="selHover">허니머스타드</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_03.png" alt="sauce_03">
+    <div class="selHover">스모크바베큐</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_04.png" alt="sauce_04">
+    <div class="selHover">홀스래디쉬</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_05.png" alt="sauce_05">
+    <div class="selHover">핫칠리</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_06.png" alt="sauce_06">
+    <div class="selHover">스위트칠리</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_07.png" alt="sauce_07">
+    <div class="selHover">스위트어니언</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_08.png" alt="sauce_08">
+    <div class="selHover">치폴레</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_sa_09.png" alt="sauce_09">
+    <div class="selHover">마요네즈</div>
+  </div>
+  <div class="select reset">
+    <img src="/burgers/img/cu_none.png" alt="sauce_none">
+    <div class="selHover">선택안함</div>
+  </div>
+</div>
+<div class="side selectBox selectBox_do" value="side">
+  <div class="select">
+    <img src="/burgers/img/cu_si_01.png" alt="side_01">
+    <div class="selHover">프렌치프라이</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_si_02.png" alt="side_02">
+    <div class="selHover">어니언링</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_si_03.png" alt="side_03">
+    <div class="selHover">치즈스틱</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_si_04.png" alt="side_04">
+    <div class="selHover">코코넛쉬림프</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_si_05.png" alt="side_05">
+    <div class="selHover">콘샐러드</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_si_06.png" alt="side_06">
+    <div class="selHover">코울슬로</div>
+  </div>
+  <div class="select reset">
+    <img src="/burgers/img/cu_none.png" alt="side_none">
+    <div class="selHover">선택안함</div>
+  </div>
+</div>
+<div class="beverage selectBox selectBox_do" value="beverage">
+  <div class="select">
+    <img src="/burgers/img/cu_be_01.png" alt="beverage_01">
+    <div class="selHover">콜라</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_be_02.png" alt="beverage_02">
+    <div class="selHover">제로콜라</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_be_03.png" alt="beverage_03">
+    <div class="selHover">스프라이트</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_be_04.png" alt="beverage_04">
+    <div class="selHover">탄산수</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_be_05.png" alt="beverage_05">
+    <div class="selHover">자몽에이드</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_be_06.png" alt="beverage_06">
+    <div class="selHover">청포도에이드</div>
+  </div>
+  <div class="select">
+    <img src="/burgers/img/cu_be_07.png" alt="beverage_07">
+    <div class="selHover">딸기에이드</div>
+  </div>
+  <div class="select reset">
+    <img src="/burgers/img/cu_none.png" alt="beverage_none">
+    <div class="selHover">선택안함</div>
+  </div>
+</div>
--- burgers/js/custom.js
+++ burgers/js/custom.js
@@ -1,4 +1,5 @@
 $(function(){
+  // tab navigation
   $('.cusNav button').click(function(){
     let selClass = $(this).attr('class')
     $('.cusNav button').removeClass('active');
@@ -8,7 +9,7 @@ $(function(){
   });
   $('.cusNav button.bun').trigger('click');
   
-
+  // insert text in pay box & select image hover when one select tab click
   $('.selectBox_on .select').click(function(){
     $(this).siblings('.select').removeClass('click');
     $(this).toggleClass('click');
@@ -23,14 +24,18 @@ $(function(){
     }
   });
 
+  // insert text in pay box & select image hover when more select tab click
   $('.selectBox_do .select').click(function(){
-    $(this).toggleClass('click');
-
     let selClass = $(this).parents('.selectBox').attr('class');
     let classNam = selClass.split(" ");
     let selNam = $(this).text();
     let paySel = $('.selList p.' + classNam[0]);
     let nowTxt = paySel.text();
+
+    $(this).siblings('.reset').removeClass('click');
+    // paySel.text('');
+
+    $(this).toggleClass('click');
     paySel.text(nowTxt + selNam);
   
     if(!$(this).hasClass('click')){
@@ -40,24 +45,70 @@ $(function(){
       let paySel = $('.selList p.' + classNam[0]);
       let nowTxt = paySel.text();
       if(nowTxt.includes(selNam)){
-        paySel.replace(selNam,'');
+        paySel.replace(selNam,''); // 한번 더 선택시 삭제 기능 추가해야함
       }
+    }
+    
+    // pay box text delete when no select btn click
+    if($(this).hasClass('reset')){
+      $(this).siblings('.select').removeClass('click');
+      $(this).addClass('click');
+      paySel.text('');
+      paySel.text(selNam);
     }
   });
 
+  // view box effect
   $('.selectBox .select').click(function(){
-    if($(this).hasClass('click')){
+    $(this).toggleClass('on');
+
+    if($(this).hasClass('on')){
     let eleIndex = $(this).index();
     let className = $(this).parents('.selectBox').attr('value');
 
-    $('.cusView .' + className + '>img').eq(eleIndex).animate({'left':'0'},300);
-    $('.cusView .' + className + '>img').eq(eleIndex).css({'position':'relative'});
-    if(className == 'bun'){
-      $('.cusView .' + className + '.bunUp>img').eq(eleIndex).animate({'left':'0'},300);
-      $('.cusView .' + className + '.bunDown>img').eq(eleIndex).animate({'left':'0'},300);
-      $('.cusView .' + className + '.bunUp>img').eq(eleIndex).css({'position':'relative'});
-      $('.cusView .' + className + '.bunDown>img').eq(eleIndex).css({'position':'relative'});
+      if($(this).parents('.selectBox.selectBox_do')){
+        $('.selectBox .select.reset').removeClass('on');
+        $(this).toggleClass('on');
+        if($('.select').hasClass('on')){
+        $('.cusView .' + className + '>img').eq(eleIndex).animate({'left':'0'},300);
+        $('.cusView .' + className + '>img').eq(eleIndex).css({'position':'relative'});
+        }
+      }
+      // remove & insert image when one select tab click
+      if($(this).parents('.selectBox.selectBox_on')){
+        $(this).siblings('.selectBox .select').removeClass('on');
+        $(this).toggleClass('on');
+        $('.cusView .' + className + '>img').css({'position':'absolute'});
+        $('.cusView .' + className + '>img').css({'left':'100%'},300);
+        $('.cusView .' + className + '>img').eq(eleIndex).animate({'left':'0'},300);
+        $('.cusView .' + className + '>img').eq(eleIndex).css({'position':'relative'});
+
+        if(className == 'bun'){
+          $('.cusView .' + className + '.bunUp>img').css({'left':'100%'});
+          $('.cusView .' + className + '.bunDown>img').css({'left':'100%'});
+          $('.cusView .' + className + '.bunUp>img').eq(eleIndex).animate({'left':'0'},300);
+          $('.cusView .' + className + '.bunDown>img').eq(eleIndex).animate({'left':'0'},300);
+          $('.cusView .' + className + '.bunUp>img').eq(eleIndex).css({'position':'relative'});
+          $('.cusView .' + className + '.bunDown>img').eq(eleIndex).css({'position':'relative'});
+        }
+      } 
     }
+
+    if(!$(this).hasClass('on')){
+      let eleIndex = $(this).index();
+      let className = $(this).parents('.selectBox').attr('value');
+      $('.cusView .' + className + '>img').eq(eleIndex).animate({'left':'100%'},300);
+      if(className == 'bun'){
+        $('.cusView .' + className + '.bunUp>img').eq(eleIndex).animate({'left':'100%'},300);
+        $('.cusView .' + className + '.bunDown>img').eq(eleIndex).animate({'left':'100%'},300);
+      }
+    }
+
+    // 다른거 선택하면 선택안함 사라지게
+    // view box image delete when no select btn click
+    if($(this).hasClass('reset')){
+      $('.selectBox .select').removeClass('on');
+      $(this).toggleClass('on');
     }
   });
 
