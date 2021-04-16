6171642d10a56d722c9cf05a86a74934a130e6d4
cocoao <cocoaooo212@gmail.com>
2021-04-17 00:25:21 +0900

    210416 info page 완료

--- burgers/pages/bug_is.php
+++ burgers/pages/bug_is.php
@@ -57,16 +57,55 @@
       </div>
       <div class="isImg useImg">
         <div class="imgBox">
+          <div class="useHover">
+            <span>통밀번</span>
+            <span>호밀번</span>
+            <span>참깨번</span>
+            <span>검은깨번</span>
+            <span>먹물번</span>
+          </div>
           <h4>BUN</h4>
           <img src="/burgers/img/is_use_01.jpg" alt="is_use_bun">
         </div>
         <div class="imgBox">
+          <div class="useHover">
+            <span>소고기</span>
+            <span>돼지고기</span>
+            <span>닭고기</span>
+            <span>새우</span>
+            <span>버섯</span>
+            <span>콩</span>
+          </div>
           <h4>PATTY</h4>
           <img src="/burgers/img/is_use_02.jpg" alt="is_use_patty">
         </div>
         <div class="imgBox">
+          <div class="useHover">
+            <span>양상추</span>
+            <span>토마토</span>
+            <span>양파</span>
+            <span>피망</span>
+            <span>베이컨</span>
+            <span>해쉬브라운</span>
+          </div>
           <h4>TOPPING</h4>
           <img src="/burgers/img/is_use_03.jpg" alt="is_use_topping">
+        </div>
+      </div>
+    </section>
+    <section class="spotBoxes isSection">
+      <div class="mapBox">
+        <div class="isTxt">
+          <div class="sectionTit">
+            <p>BURGUR'S <i>spot</i></p>
+          </div>
+          <form action="#" class="spotBox">
+            <input type="text">
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
+        <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101219.84507526795!2d126.81636417547168!3d37.552232734058634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9c06a939f9fd%3A0x7403cca890b034f8!2z67KE6rGw7YK5IOqwgOyWkeydtOuniO2KuA!5e0!3m2!1sko!2skr!4v1618584189034!5m2!1sko!2skr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
       </div>
     </section>
